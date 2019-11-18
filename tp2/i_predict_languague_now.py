70de5f7f6856b59d98f09b8df58a301bdc3a9305
Luís Alves <luismig.alves@gmail.com>
2019-11-18 18:15:25 +0000

    Improved naming of variables, added test text files, added a better output

--- tp2/i_predict_languague_now.py
+++ tp2/i_predict_languague_now.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Ngrams - Calculate ngrams frequency
+I Predict Language Now (IPLN) - detects the language of a txt file, and improves
+our cool db
 """
 
 import re
@@ -12,19 +13,32 @@ from collections import Counter
 import shelve
 #import counter
 
-cnt = Counter() # counter
-
-def find_la(texto, filename):
+def find_lang(texto, filename, language_counter):
+    num_w = 0
     d = shelve.open( filename )  # open -- file may get suffix added by low-level
-    for w in texto.split(" "): # run through every word of the text
-        if w in d: # check if the word exists in the dictonaries
-            for i in d[w]: # run through the list of languages
-                cnt[i] += 1
+    for word in texto.split(" "): # run through every word of the text
+        num_w += 1
+        if word in d: # check if the word exists in the dictonaries
+            for language in d[word]: # run through the list of languages
+                language_counter[language] += 1
     d.close()
+    return num_w
+
+# returns the percentage of the winner and closest loser language vs number of words
+def print_stats(language_counter, num_words):
+    top_2 = language_counter.most_common(2)
+    percentage_of_winner_lang = top_2[0][1] / num_words * 100
+    percentage_of_loser_lang = top_2[1][1] / num_words * 100
+    print(f"I believe this text is in {top_2[0][0]}! I'm {percentage_of_winner_lang}% sure!")
+    if percentage_of_loser_lang > 50:
+        print(f"However, it was a close call! {top_2[1][0]} seems like a close fit, with {percentage_of_loser_lang}% of matching words!")
+    return percentage_of_winner_lang, percentage_of_loser_lang
 
 def main():
     opts, resto = getopt(sys.argv[1:],"")
     dop = dict(opts)
+
+    cnt = Counter() # counter by language
 
     print(resto)
     texto = ""
@@ -33,8 +47,9 @@ def main():
     for line in fileinput.input(resto[1]):
         texto += line
 
-    find_la(texto,resto[0])
-    print(cnt.most_common(1))
-
+    num_words = find_lang(texto,resto[0], cnt)
+    percent_win, percent_los = print_stats(cnt, num_words)
+    # if percentage is something something, add to dictionary
+    # TODO
 
 main()
