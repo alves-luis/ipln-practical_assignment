f4f0fb1736f26bc3b43e4d8bc1a78c67479544ff
Diogo Gonçalves <diogo.goncaalves@gmail.com>
2019-11-30 15:12:52 +0000

    New add to dictionary functionality and got a better regex, still needs some testes

--- tp2/i_predict_languague_now.py
+++ tp2/i_predict_languague_now.py
@@ -1,27 +1,25 @@
-#!/usr/bin/python3
+#!/usr/local/bin/python3
+
 """
 I Predict Language Now (IPLN) - detects the language of a txt file, and improves
 our cool db
 """
 
 import re
-import subprocess
 import sys
 from getopt import getopt
 import fileinput
 from collections import Counter
 import shelve
-#import counter
 
-def find_lang(texto, filename, language_counter):
+def find_lang(texto, d, language_counter):
     num_w = 0
-    d = shelve.open( filename )  # open -- file may get suffix added by low-level
-    for word in texto.split(" "): # run through every word of the text
+    for word in [i for i in re.split(r'[\s\t\n,.?!]',texto) if i]: # run through every word of the text
         num_w += 1
         if word in d: # check if the word exists in the dictonaries
+            current_filtered = []
             for language in d[word]: # run through the list of languages
                 language_counter[language] += 1
-    d.close()
     return num_w
 
 # returns the percentage of the winner and closest loser language vs number of words
@@ -34,22 +32,49 @@ def print_stats(language_counter, num_words):
         print(f"However, it was a close call! {top_2[1][0]} seems like a close fit, with {percentage_of_loser_lang}% of matching words!")
     return percentage_of_winner_lang, percentage_of_loser_lang
 
+
+def add_text_to_dict(d,texto,percent_win,language_to_add, resto):
+    for e in range(len(resto)):
+        if resto[e] == '-A':
+            percentage_to_add = resto[e+1]
+
+    if percent_win > int(percentage_to_add):
+        for word in re.split(r'[\s\t\n,.?!]',texto):
+            # Check if the word exists in the dictionary
+            if word in d:
+                # Check if the word in the dictionary has the language_to_add
+                if language_to_add not in d[word]:
+                    var = d[word];
+                    var.append( language_to_add )
+                    d[word] = var
+                else:
+                    # If word doesn't exist in the dictionary add it
+                    d[word] = [language_to_add]
+
+
 def main():
-    opts, resto = getopt(sys.argv[1:],"")
+    opts, resto = getopt(sys.argv[1:],"A")
     dop = dict(opts)
 
     cnt = Counter() # counter by language
 
-    print(resto)
     texto = ""
-
     # Reads file
     for line in fileinput.input(resto[1]):
         texto += line
 
-    num_words = find_lang(texto,resto[0], cnt)
+    d = shelve.open( resto[0]) # open -- file may get suffix added by low-level
+
+    num_words = find_lang(texto, d , cnt)
+    print(num_words)
+    print(cnt.most_common(2))
     percent_win, percent_los = print_stats(cnt, num_words)
-    # if percentage is something something, add to dictionary
-    # TODO
+
+    language_to_add = cnt.most_common(1)[0][0]
+
+    if '-A' in resto:
+        add_text_to_dict(d,texto,percent_win,language_to_add,resto)
+
+    d.close()
 
 main()
